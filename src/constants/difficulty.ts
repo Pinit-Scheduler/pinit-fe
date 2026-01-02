067bbbb2c026ef339e469619c5c8026b7f3de651
GoGradually <chkun3109@gmail.com>
2026-01-03 02:05:44 +0900

    feat: 기본 난이도를 5에서 2로 변경

--- src/constants/difficulty.ts
+++ src/constants/difficulty.ts
@@ -2,7 +2,7 @@ export const FIBONACCI_DIFFICULTIES = [1, 2, 3, 5, 8, 13, 21] as const
 
 export type DifficultyValue = typeof FIBONACCI_DIFFICULTIES[number]
 
-export const DEFAULT_DIFFICULTY: DifficultyValue = 5
+export const DEFAULT_DIFFICULTY: DifficultyValue = 2
 
 export const isValidDifficulty = (value: number): value is DifficultyValue =>
   FIBONACCI_DIFFICULTIES.includes(value as DifficultyValue)
