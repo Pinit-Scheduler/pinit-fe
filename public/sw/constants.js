1438db346efa5736bf301fd6063e95095061934c
GoGradually <chkun3109@gmail.com>
2026-02-08 14:02:22 +0900

    feat: 프리캐시 URL 목록에 추가 리소스 포함

--- public/sw/constants.js
+++ public/sw/constants.js
@@ -1,8 +1,20 @@
 self.pinitSw = self.pinitSw || {}
 
 self.pinitSw.constants = {
-  CACHE_NAME: 'pinit-cache-v1',
-  PRECACHE_URLS: ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg', '/vite.svg'],
+  CACHE_NAME: 'pinit-cache-v1', //TODO CACHE_NAME 자동으로 올릴 기법
+  PRECACHE_URLS: [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/icons/icon.svg',
+    '/vite.svg',
+    '/sw/constants.js',
+    '/sw/cache.js',
+    '/sw/time.js',
+    '/sw/idempotency.js',
+    '/sw/push.js',
+    '/sw/clients.js',
+  ],
   KST_TIMEZONE: 'Asia/Seoul',
   IDEMPOTENT_DB_NAME: 'pinit-notifications',
   IDEMPOTENT_STORE_NAME: 'recent-idempotent-keys',
